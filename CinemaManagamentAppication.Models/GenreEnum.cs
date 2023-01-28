c0346574aa9390d1cf6c982fe1a4e2a5730aaf2f
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-28 17:28:56 +0100

    Add GenreEnum with options

---
+++ CinemaManagamentAppication.Models/GenreEnum.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaManagamentAppication.Models
+{
+    public enum GenreEnum
+    {
+        Action,
+        SciFi,
+        Drama,
+        Comedy,
+        Thriler
+    }
+}
--- CinemaManagamentAppication.Models/Movie.cs
+++ CinemaManagamentAppication.Models/Movie.cs
@@ -6,7 +6,7 @@ namespace CinemaManagamentAppication.Models
     {
         public string Title { get; set; }
         public int Duration { get; set; }
-        public string Genre { get; set; }
+        public GenreEnum Genre { get; set; }
         public decimal Price { get; set; }
 
 
