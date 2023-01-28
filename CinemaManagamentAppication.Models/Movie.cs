10bed3e27072c2e94d2e4bfa450ece0cd9f595f6
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-28 17:23:01 +0100

    Create new proect with class library for models,adding class movie.

---
+++ CinemaManagamentAppication.Models/Movie.cs
@@ -0,0 +1,14 @@
+﻿
+
+namespace CinemaManagamentAppication.Models
+{
+    public class Movie
+    {
+        public string Title { get; set; }
+        public int Duration { get; set; }
+        public string Genre { get; set; }
+        public decimal Price { get; set; }
+
+
+    }
+}
