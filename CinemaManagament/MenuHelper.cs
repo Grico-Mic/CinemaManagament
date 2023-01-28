dd0fceb33cd302ec9dc10300158eef255ea596cd
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-28 18:56:03 +0100

    Add class MenuHelper

---
+++ CinemaManagament/MenuHelper.cs
@@ -0,0 +1,60 @@
+﻿using System;
+
+
+namespace CinemaManagament
+{
+    public class menuHelper
+    {
+        public  void ShowEditOptions()
+        {
+            Console.WriteLine("Please choose one of the following options");
+            Console.WriteLine("1. Hall");
+            Console.WriteLine("2. Movie");
+            Console.WriteLine("3. Snack");
+
+            var editUserInput = Console.ReadLine();
+
+            switch (editUserInput)
+            {
+                case "1":
+                    Console.WriteLine("Hall");
+                    break;
+                case "2":
+                    ShowMovieEditOptions();
+                    break;
+                case "3":
+                    Console.WriteLine("Snack");
+                    break;
+
+                default:
+                    Console.WriteLine("Invalid input");
+                    break;
+            }
+        }
+
+        public void ShowMovieEditOptions()
+        {
+            Console.WriteLine("Please choose one of the following options");
+            Console.WriteLine("Create");
+            Console.WriteLine("Delete");
+            Console.WriteLine("Edit price");
+
+            var editMovieUserInput = Console.ReadLine().Trim();
+
+            switch (editMovieUserInput)
+            {
+                case "1":
+                    Console.WriteLine("Create movie");
+                    break;
+                case "2":
+                    Console.WriteLine("Delete movie");
+                    break;
+                case "3":
+                    Console.WriteLine("Change price for movie");
+                    break;
+
+            }
+
+        }
+    }
+}
