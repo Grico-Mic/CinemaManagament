3aa4bce353c71bce351e9c129e9a7cd86f7c8b5a
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-29 19:07:31 +0100

    Create new project CinemaManagamentServise

---
+++ CinemaManagament.Servise/CinemaManagamentServise.cs
@@ -0,0 +1,22 @@
+﻿
+
+using System;
+
+namespace CinemaManagament.Servise
+{
+    public class CinemaManagamentServise
+
+    {
+        public void CreateMovie()
+        {
+            Console.WriteLine("Please enter movie title");
+            var userInputMovieTitle = Console.ReadLine();
+            Console.WriteLine("Please enter duration");
+            var userInputMovieDuration = Console.ReadLine();
+            Console.WriteLine("Please enter movie genre");
+            var userInputMovieGenre = Console.ReadLine();
+        }
+  
+    }
+}
+
