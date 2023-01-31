45b55a02ca7162ab695b191d88d292e3ef30ac74
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-31 20:50:27 +0100

    Add new property Id

--- CinemaManagamentAppication.Models/Movie.cs
+++ CinemaManagamentAppication.Models/Movie.cs
@@ -4,11 +4,10 @@ namespace CinemaManagamentAppication.Models
 {
     public class Movie
     {
+        public int Id { get; set; }
         public string Title { get; set; }
         public int Duration { get; set; }
         public GenreEnum Genre { get; set; }
         public decimal Price { get; set; }
-
-
     }
 }
