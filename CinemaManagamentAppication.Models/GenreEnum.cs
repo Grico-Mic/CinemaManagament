2ddd727548144489c29f0dd92b0f6e2198a1dd98
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-01-31 20:25:34 +0100

    add numbers to enum

--- CinemaManagamentAppication.Models/GenreEnum.cs
+++ CinemaManagamentAppication.Models/GenreEnum.cs
@@ -6,10 +6,10 @@ namespace CinemaManagamentAppication.Models
 {
     public enum GenreEnum
     {
-        Action,
-        SciFi,
-        Drama,
-        Comedy,
-        Thriler
+        Action = 1,
+        SciFi = 2,
+        Drama = 3,
+        Comedy = 4,
+        Thriler = 5
     }
 }
