4026ab46cf1393d87333ef41fbd025478c2df577
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-01 21:49:16 +0100

    Add CRUD operations in MovieRepository.cs

--- CinemaManagament.Repositories/MovieRepository.cs
+++ CinemaManagament.Repositories/MovieRepository.cs
@@ -1,8 +1,36 @@
-﻿using System;
+﻿using CinemaManagamentAppication.Models;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace CinemaManagament.Repositories
 {
     public class MovieRepository
     {
+        public MovieRepository()
+        {
+            Movies = new List<Movie>();
+        }
+        private List<Movie> Movies { get; set; }
+
+        public void Create(Movie movie)
+        {
+            Movies.Add(movie);
+        }
+
+        public void Delete(Movie movie)
+        {
+            Movies.Remove(movie);
+        }
+
+        public List<Movie> GetAll()
+        {
+            return Movies;
+        }
+
+        public Movie GetById(int id)
+        {
+            return Movies.FirstOrDefault(x => x.Id == id);
+        }
+
     }
 }
