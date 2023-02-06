4cc8f534aa2605b498a31995865a4d8efc540c1d
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-06 18:00:38 +0100

    Add new project Common and folder Exceptions

---
+++ CinemaManagament.Common/Exceptions/CinemaManagamentExceptions.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace CinemaManagament.Common.Exceptions
+{
+    public class CinemaManagamentExceptions : Exception
+    {
+        public CinemaManagamentExceptions(string message):base(message)
+        {
+
+        }
+    }
+}
