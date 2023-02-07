5855f84cd8ef254e5f9e2838911f641e2e8d327b
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-07 18:06:56 +0100

    Add new folder in project Common with class that inside have method to validate String.IsNullOrEmpty

---
+++ CinemaManagament.Common/Validators/StringValidator.cs
@@ -0,0 +1,20 @@
+﻿using CinemaManagament.Common.Exceptions;
+using System;
+
+
+namespace CinemaManagament.Common.Validators
+{
+    public class StringValidator
+    {
+        public string CheeckNullOrEmpty(string input)
+        {
+          
+
+            if (String.IsNullOrEmpty(input))
+            {
+                throw new CinemaManagamentExceptions("Invalid Input");
+            }
+            return input;
+        }
+    }
+}
