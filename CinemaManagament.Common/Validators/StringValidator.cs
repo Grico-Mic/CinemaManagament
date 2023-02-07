6c390dd42fc7ea71ce03e5801f486bcad48eff9c
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-07 19:55:21 +0100

    Move method ValidatePositive to class StringValidator and delete class IntegerValidator.

--- CinemaManagament.Common/Validators/StringValidator.cs
+++ CinemaManagament.Common/Validators/StringValidator.cs
@@ -16,5 +16,18 @@ namespace CinemaManagament.Common.Validators
             }
             return input;
         }
+
+        public static int ValidatePositive(string input)
+        {
+            var rezult = int.TryParse(input, out int parsed);
+
+            if (!rezult || parsed < 0)
+            {
+                throw new CinemaManagamentExceptions("Invalid input");
+            }
+            return parsed;
+        }
     }
+
+    
 }
