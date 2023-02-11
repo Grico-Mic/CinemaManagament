63d9828ca364546e544848dcbe1cb43984f2452b
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-11 21:56:45 +0100

    Create new repository for products with methods.

---
+++ CinemaManagament.Repositories/ProductRepository.cs
@@ -0,0 +1,45 @@
+﻿using CinemaManagamentAppication.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace CinemaManagament.Repositories
+{
+    public class ProductRepository
+    {
+        public ProductRepository()
+        {
+
+            Products = new List<Product>();
+        }
+        private List<Product> Products { get; set; }
+
+        public void CreateProduct(Product newProduct)
+        {
+            newProduct.Id = GenerateProductId();
+            Products.Add(newProduct);
+        }
+
+        public Product GetByName(string userInputProductName)
+        {
+            return Products.FirstOrDefault(x => x.Name == userInputProductName);
+        }
+
+        private int GenerateProductId()
+        {
+
+            {
+                var newId = 0;
+
+                if (Products.Count > 0)
+                {
+                    newId = Products.Max(x => x.Id);
+
+                }
+                return newId + 1;
+            }
+        }
+    }
+}
+
