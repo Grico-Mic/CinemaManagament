a07b115b6a8cc1afddd5549a73dbbc2de9e8296b
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-20 20:34:17 +0100

    Implement seeding for populate database with data that is required  to start with testing.

--- CinemaManagament.Repositories/MovieRepository.cs
+++ CinemaManagament.Repositories/MovieRepository.cs
@@ -9,7 +9,13 @@ namespace CinemaManagament.Repositories
     {
         public MovieRepository()
         {
-            _movies = new List<Movie>();
+            
+            _movies = new List<Movie>() 
+            {
+                new Movie(){Id = 1,Title = "Shawshank Redemption",Duration = 140,Genre = GenreEnum.Drama,Price = 1000},
+                new Movie(){Id = 2,Title = "Wind River",Duration = 120,Genre = GenreEnum.Action,Price = 800},
+                new Movie(){Id = 3,Title = "The boy who harnessed the wind",Duration = 120,Genre = GenreEnum.Drama,Price = 600}
+             };
         }
         private List<Movie> _movies { get; set; }
 
--- CinemaManagament.Repositories/ProductRepository.cs
+++ CinemaManagament.Repositories/ProductRepository.cs
@@ -11,7 +11,11 @@ namespace CinemaManagament.Repositories
         public ProductRepository()
         {
 
-            _products = new List<Product>();
+            _products = new List<Product>() 
+            {
+                new Product(){Id = 1,Name = "Skopsko", Price = 120, Quantity=85},
+                new Product(){Id = 2, Name = "Coca Cola", Price = 80, Quantity = 63}
+            };
         }
         private List<Product> _products { get; set; }
 
