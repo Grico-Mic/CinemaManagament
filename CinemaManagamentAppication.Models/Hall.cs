cf350930db17cd792e75aaa757b34060fbc183ae
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-21 21:37:32 +0100

    Create class with properties for option Hall

---
+++ CinemaManagamentAppication.Models/Hall.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CinemaManagamentAppication.Models
+{
+    public class Hall : BaseEntity
+    {
+        public string Name { get; set; }
+
+        public int NumberOfSeads { get; set; }
+        public int MovieId { get; set; }
+    }
+}
