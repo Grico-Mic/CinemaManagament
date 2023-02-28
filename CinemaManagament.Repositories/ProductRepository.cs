d0433b78f188f79bbce9c4533501e89de689d28b
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-28 19:06:57 +0100

    remove property

--- CinemaManagament.Repositories/ProductRepository.cs
+++ CinemaManagament.Repositories/ProductRepository.cs
@@ -11,19 +11,17 @@ namespace CinemaManagament.Repositories
         public ProductRepository()
         {
 
-            _products = new List<Product>() 
+            Data = new List<Product>() 
             {
                 new Product(){Id = 1,Name = "Skopsko", Price = 120, Quantity=85},
                 new Product(){Id = 2, Name = "Coca Cola", Price = 80, Quantity = 63}
             };
         }
-        private List<Product> _products { get; set; }
-
        
 
         public Product GetByName(string userInputProductName)
         {
-            return _products.FirstOrDefault(x => x.Name == userInputProductName);
+            return Data.FirstOrDefault(x => x.Name == userInputProductName);
         }
 
     }
