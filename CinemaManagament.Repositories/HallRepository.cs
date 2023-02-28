9eb37ca69c76268cf7fded7a238ca89602542ac2
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-28 18:20:40 +0100

    Create HallRepository with method for creatig a Hall

---
+++ CinemaManagament.Repositories/HallRepository.cs
@@ -0,0 +1,23 @@
+﻿using CinemaManagament.Repositories.Common;
+using CinemaManagamentAppication.Models;
+using System.Collections.Generic;
+
+
+namespace CinemaManagament.Repositories
+{
+    public class HallRepository
+    {
+        public HallRepository()
+        {
+            _halls = new List<Hall>();
+        }
+
+        private List<Hall> _halls { get; set; }
+
+        public void Create(Hall hall)
+        {
+            hall.Id = IdGenerator.GenerateId(_halls);
+            _halls.Add(hall);
+        }
+    }
+}
