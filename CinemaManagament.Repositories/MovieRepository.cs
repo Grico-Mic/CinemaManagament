1fe6a2a36084ede3858af44885d3d5ed4b3980a0
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-02-28 18:56:42 +0100

    Remove common method's to BaseRepostory and add inheritance.

--- CinemaManagament.Repositories/MovieRepository.cs
+++ CinemaManagament.Repositories/MovieRepository.cs
@@ -1,45 +1,19 @@
-﻿using CinemaManagament.Repositories.Common;
-using CinemaManagamentAppication.Models;
+﻿using CinemaManagamentAppication.Models;
 using System.Collections.Generic;
-using System.Linq;
 
 namespace CinemaManagament.Repositories
 {
-    public class MovieRepository
+    public class MovieRepository : BaseRepository<Movie>
     {
         public MovieRepository()
         {
             
-            _movies = new List<Movie>() 
+            Data = new List<Movie>() 
             {
                 new Movie(){Id = 1,Title = "Shawshank Redemption",Duration = 140,Genre = GenreEnum.Drama,Price = 1000},
                 new Movie(){Id = 2,Title = "Wind River",Duration = 120,Genre = GenreEnum.Action,Price = 800},
                 new Movie(){Id = 3,Title = "The boy who harnessed the wind",Duration = 120,Genre = GenreEnum.Drama,Price = 600}
              };
         }
-        private List<Movie> _movies { get; set; }
-
-        public void Create(Movie movie)
-        {
-            movie.Id = IdGenerator.GenerateId<Movie>(_movies);
-            _movies.Add(movie);
-        }
-
-        public void Delete(Movie movie)
-        {
-            _movies.Remove(movie);
-        }
-
-            public List<Movie> GetAll()
-        {
-            return _movies;
-        }
-
-            public Movie GetById(int id)
-        {
-            return _movies.FirstOrDefault(x => x.Id == id);
-        }
-
-       
     }
 }
--- CinemaManagament.Repositories/ProductRepository.cs
+++ CinemaManagament.Repositories/ProductRepository.cs
@@ -6,7 +6,7 @@ using System.Linq;
 
 namespace CinemaManagament.Repositories
 {
-    public class ProductRepository
+    public class ProductRepository : BaseRepository<Product>
     {
         public ProductRepository()
         {
@@ -19,29 +19,13 @@ namespace CinemaManagament.Repositories
         }
         private List<Product> _products { get; set; }
 
-        public void CreateProduct(Product newProduct)
-        {
-            newProduct.Id = IdGenerator.GenerateId<Product >(_products);
-            _products.Add(newProduct);
-        }
-
-        public List<Product> GetAll()
-        {
-            return _products;
-        }
+       
 
         public Product GetByName(string userInputProductName)
         {
             return _products.FirstOrDefault(x => x.Name == userInputProductName);
         }
 
-        public void Remove(Product choosenProduct)
-        {
-            _products.Remove(choosenProduct);
-        }
-        
-
-        
     }
 }
 
