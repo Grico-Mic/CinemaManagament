2e0be13d7e0908f5abda79a6dceff142de4d5838
Grico-Mic <107273766+Grico-Mic@users.noreply.github.com>
2023-03-25 00:13:23 +0100

    Create classs with properties.

---
+++ CinemaManagamentAppication.Models/Receipt.cs
@@ -0,0 +1,19 @@
+﻿
+
+using System;
+using System.Collections.Generic;
+
+namespace CinemaManagamentAppication.Models
+{
+    public  class Receipt : BaseEntity
+    {
+        public List<Product> Products { get; set; }
+        public int TicketQuantity { get; set; }
+        public string HallName { get; set; }
+        public string MovieTitle { get; set; }
+        public decimal PricePerTicket { get; set; }
+        public decimal TotalPrice { get; set; }
+        public string CustomerName { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
